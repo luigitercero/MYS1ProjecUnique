cb536b833a373909f06c9ceeba035df5a0ad25d4
Luis Azurdia <luigitercero>
2018-10-31 22:15:54 -0600

    distribucion aeropuetors

--- ProyectoMYS1Final/Form1.cs
+++ ProyectoMYS1Final/Form1.cs
@@ -1,4 +1,5 @@
-﻿using SimioAPI;
+﻿using ProyectoMYS1Final.SIMIO;
+using SimioAPI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,8 @@ namespace ProyectoMYS1Final
 {
     public partial class Form1 : Form
     {
+
+
         static ISimioProject _simioproyecto;
         String _rutaBase = Application.StartupPath + "/SimioModel.spfx";
         String _rutaSalida = Application.StartupPath + "/final/SimioShow.spfx";
@@ -33,10 +36,10 @@ namespace ProyectoMYS1Final
             _modelo = _simioproyecto.Models[1];
 
 
-            crear();
+            //crear();
 
-
-            guardarModeloSalida();
+   
+            //guardarModeloSalida();
 
         }
         private void guardarModeloSalida() {
@@ -50,8 +53,58 @@ namespace ProyectoMYS1Final
             int y = 1;
             IIntelligentObject combiner = _objetos.CreateObject("Combiner", new FacilityLocation(x, z, y));
             combiner.ObjectName = "fabi";
-
+            INodeObject input = seekForName("MemberInput@" + combiner.ObjectName) as INodeObject;
+            INodeObject output = seekForName("ParentInput@" + combiner.ObjectName) as INodeObject;
+            List<FacilityLocation> listpoints = new List<FacilityLocation>();
+            FacilityLocation fl = new FacilityLocation(35, 36, 86);
+            listpoints.Add(fl);
+            IIntelligentObject connect1 = _objetos.CreateLink("Connector", input, output, listpoints);
         }
 
+        private IIntelligentObject seekForName(String name) {
+            //IIntelligentObject entity = null;
+            IIntelligentObjects list = _modelo.Facility.IntelligentObjects;
+            foreach (var entity in list)
+            {
+                if (entity.ObjectName == name) {
+                    return entity;
+                }
+            }
+            return null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv fieles(*.csv)|*.csv|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            //ReadCsv read = new ReadCsv();
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            Main main = new Main();
+
+            main.start(filePath);
+        }
     }
 }
---
+++ ProyectoMYS1Final/SIMIO/main.cs
@@ -0,0 +1,55 @@
+﻿using SimioAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMYS1Final.SIMIO
+{
+    class Main
+    {
+        private SimioFile simio;
+        ReadCsv read;
+        Dictionary <int,IIntelligentObject> ariport;
+        
+        public Main() {
+            ariport = new Dictionary<int, IIntelligentObject>();
+
+        }
+
+        internal void start(string filePath)
+        {
+            simio = new SimioFile();
+            read = new ReadCsv(filePath,',');
+            string[] line = read.getline();
+            line = read.getline();
+            IIntelligentObject airplane = simio.createSource("airplane", 0, 0, 0);
+            IIntelligentObject person = simio.createSource("person", 2, 0, 0);
+            INodeObject airplaneOutput = simio.getNodeOutput(airplane);
+            INodeObject personOutput = simio.getNodeOutput(person);
+
+            simio.createSink("salida", 1, 1, 1);
+            while (line != null) {
+                createAirPort(line, airplaneOutput, personOutput);
+                line = read.getline();
+            }
+
+            simio.saveFile();
+        }
+        private void createAirPort(String[] line, INodeObject airplane, INodeObject person)
+        {
+
+            IIntelligentObject _object = simio.createCombiner(line[1], Int16.Parse(line[2]), Int16.Parse(line[3]), Int16.Parse(line[4]));
+            this.ariport.Add(Int16.Parse(line[0]),_object);
+            INodeObject parent = this.simio.getParentIpunt(_object);
+            INodeObject member = this.simio.getMemberIpunt(_object);
+          
+            IIntelligentObject connect1 = this.simio.getObjectList().CreateLink("Connector",airplane, parent, null);
+            IIntelligentObject connect2 = this.simio.getObjectList().CreateLink("Connector",  person, member, null);
+
+        }
+    }
+
+   
+}
